6ad726ba4569fffed6bbede51260f19fb3e4d37f
Dana Ainiwaer <danaanwer@outlook.com>
2026-02-04 21:00:11 +0900

    Initial Commit

---
+++ Gainday/GaindayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaindayApp.swift
+//  Gainday
+//
+//  Created by yanmengru on 2026/02/04.
+//
+
+import SwiftUI
+
+@main
+struct GaindayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GaindayTests/GaindayTests.swift
@@ -0,0 +1,16 @@
+//
+//  GaindayTests.swift
+//  GaindayTests
+//
+//  Created by yanmengru on 2026/02/04.
+//
+
+import Testing
+
+struct GaindayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
